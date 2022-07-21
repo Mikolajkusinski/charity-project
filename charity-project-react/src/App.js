8ac7844c1a2301b12e05f2fc6e9cf0e8ef7258cf
Mikolajkusinski <77103765+Mikolajkusinski@users.noreply.github.com>
2022-07-21 09:46:02 +0200

    improvement added to navbar and map

--- charity-project-react/src/App.js
+++ charity-project-react/src/App.js
@@ -8,15 +8,18 @@ import {
 import { formatRelative} from "date-fns";
 import mapStyles from "./mapStyles";
 import NavBar from "./navBar";
+import usePlacesAutocomplete, {getGeocode, getLatLng} from "use-places-autocomplete";
+import {Combobox, ComboboxInput, ComboboxOption, ComboboxPopover} from "@reach/combobox";
+import "@reach/combobox/styles.css"
 
 const libraries = ["places"];
 const mapContainerStyle = {
     width: "100vw",
-    height: "100vh",
+    height: "93vh",
 };
 const center = {
-    lat: 43,
-    lng: -79,
+    lat: 54,
+    lng: 18.5,
 };
 
 const options = {
@@ -25,11 +28,23 @@ const options = {
     zoomControl: true,
 }
 
+
+
 export default function App(){
     const {isLoaded, loadError} = useLoadScript(
         {googleMapsApiKey: "AIzaSyDg-IC3oUW9Wb0I_KXleLy1JIavyPDUBe0",
         libraries,
         });
+
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) =>{
+        mapRef.current = map;
+    },[]);
+
+    const panTo = React.useCallback(({lat,lng}) => {
+        mapRef.current.panTo({lat,lng});
+        mapRef.current.setZoom(14);
+    },[]);
 
     if(loadError) return "ERROR loading maps";
     if(!isLoaded) return "Loading Maps...";
@@ -39,9 +54,52 @@ export default function App(){
     return(
 
         <div>
+            <Search panTo = {panTo}/>
             <NavBar/>
-            <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center} options={options}></GoogleMap>
+
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={8}
+                center={center}
+                options={options}
+               onLoad={onMapLoad}
+            ></GoogleMap>
         </div>
     )
 
+}
+
+function Search({panTo}){
+    const {ready, value, suggestions: {status, data}, setValue , clearSuggestions} = usePlacesAutocomplete({
+        requestOptions:{
+            location: { lat: () => 54, lng: () => 18.5},
+            radius: 200 *1000,
+        },
+    });
+
+
+
+    return<div className={"search"}> <Combobox onSelect={ async (address) =>{
+        try {
+            const results = await getGeocode({address});
+            const {lat, lng} = await getLatLng(results[0]);
+            panTo({lat,lng});
+        }catch (error) {
+            console.log("error!");
+        }
+    }}>
+        <ComboboxInput
+            value = {value}
+            onChange={(e) =>{
+                setValue(e.target.value);
+            }}
+            disabled = {!ready}
+            placeholder={"Podaj adres"}
+        />
+        <ComboboxPopover>
+            {status === "OK" && data.map(({id,description}) =>(<ComboboxOption key = {id} value={description}/>))}
+
+        </ComboboxPopover>
+    </Combobox>
+    </div>
 }
--- charity-project-react/src/navBar.js
+++ charity-project-react/src/navBar.js
@@ -1,13 +1,17 @@
 export default function NavBar(){
+
+
     return(
         <nav className={"nav"}>
-            <a href={"/"} className={"title"}>Miejsca Pamięci</a>
-
+            <a href={"/"} className={"title"}>Miejsca Pamięci <span role={"img"} aria-label={"znicz"}>🪔</span></a>
             <ul>
                 <li>
-                    Test
+                    <a href={"/"}>Sign in</a>
                 </li>
             </ul>
         </nav>
     );
 }
+
+
+
